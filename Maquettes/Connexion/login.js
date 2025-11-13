23a6b9064b91ecbaa01815d45bbcd19be9de4331
Sben2000 <soufyane2000@hotmail.fr>
2025-11-13 12:09:05 +0100

    maquette signUp.js done

---
+++ Maquettes/Connexion/login.js
@@ -0,0 +1,12 @@
+
+/*******Montrer le mot de passe de la page Login**************************/
+const checkbox = document.getElementById("passCheckbox");
+const passInput1 = document.getElementById("pass1");//querySelectorAll("input[type=password]"); --> n'arrive pas à tous les modifier avec une seule query (même avec une classe commune) donc décomposé en id
+
+checkbox.addEventListener("change",()=>{
+    if(checkbox.checked){      //si la checkbox est coché (checked)        
+        passInput1.type="text";// "change le type de l'input password en text"
+    }else{
+        passInput1.type="password";
+    }
+})
---
+++ Maquettes/Connexion/signUP.js
@@ -0,0 +1,21 @@
+
+
+
+/*******Montrer les mots de passes du formulaire d'enregistrement SignUp**************************/
+const checkbox = document.getElementById("passCheckbox");
+const passInput1 = document.getElementById("pass1");//querySelectorAll("input[type=password]"); --> n'arrive pas à tous les modifier avec une seule query (même avec une classe commune) donc décomposé en id
+const passInput2 = document.getElementById("pass2");
+
+
+checkbox.addEventListener("change",()=>{
+    if(checkbox.checked){      //si la checkbox est coché (checked)        
+        passInput1.type="text";// "change le type de l'input password en text"
+        passInput2.type="text";  
+
+    }else{
+        passInput1.type="password";
+        passInput2.type="password";
+
+    }
+})
+
