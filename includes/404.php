1dd6c8cc51623d4ff487d7a18d03e45385723df7
Sben2000 <soufyane2000@hotmail.fr>
2025-12-05 14:32:40 +0100

    ajout page 404.php et redirections

---
+++ includes/404.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!--import des fonts  lobster et montserrat depuis googlefonts-->
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+		rel="stylesheet" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
+		rel="stylesheet" />
+
+	<!--link css 404 -->
+	<link rel="stylesheet" href="./allUsers/CSS/connexion.css" />
+
+</head>
+<body>
+	<div class="main">
+		<div class="separator">
+			<h1 class="sectionTitle">404</h1>
+		</div>
+
+		<section class="Section">
+			<div class="SectionContent">
+				<h2>Page non trouvée :-(</h2>
+					<p> Désolé nous ne trouvons pas la page demandée ... </p><br>
+					<p>Veuillez réessayer depuis la page d'accueil:</p><br>
+					<p style="text-align: end;">Revenir à la page d'<a href="index.php">Accueil</a></p>
+
+			</div>
+		</section>
+	</div>
+</body>
+
+</html>
