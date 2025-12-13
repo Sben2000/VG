716babec2e3675161ddbaa1f1c938d4b1d885e6a
Sben2000 <soufyane2000@hotmail.fr>
2025-12-13 20:28:56 +0100

    Mise en forme gestion Libellé nav  suite

--- 2_vgTeam/gestionLibelles/includes/navbar.php
+++ 2_vgTeam/gestionLibelles/includes/navbar.php
@@ -1,18 +1,12 @@
     <link href="../css/style.css"	rel="stylesheet">
 
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary ">
-    <div class="collapse navbar-collapse " id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-        <li class="nav-item">   <!--amène vers liste de regime MVC....php via le routage  action=list dans index.php -->
-          <a class="nav-link" aria-current="page" href="../regimeMVC/index.php?action=list">"Regime"</a>
-        </li>
-        <li class="nav-item"> <!--amène vers liste de theme MVC....php via le routage  action=list dans index.php -->
-        <a class="nav-link" aria-current="page" href="../themeMVC/index.php?action=list">"Theme"</a>
-        </li>
-      </ul>
-    </div>
-  </div>
+
+<header>
+<nav class="large-menu">
+<div class="inlineLinks">
+				<li><a href="../themeMVC/index.php?action=list">Thèmes</a></li>
+				<li><a href="../regimeMVC/index.php?action=list">Régimes</a></li>
+				<li><a href="./contact.php">Allergènes</a></li>
 </nav>
-
-
+</header>
